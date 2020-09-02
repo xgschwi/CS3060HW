1796686d90cde9c65b66b92da552b1ea40d00f4c
Sankardas Roy <sanroy@bgsu.edu>
2020-09-02 00:06:16 -0400

    started

---
+++ hw1/task.rb
@@ -0,0 +1,39 @@
+def function1
+    # Complete this function
+   
+end
+
+def function2
+    # Complete this function
+   
+end
+
+def function3
+    # Complete this function
+   
+end
+
+def function4
+    # Complete this function
+   
+end
+
+def function5
+ # Complete this function
+ aFile = File.new("story.txt", "r")
+ words = []
+ freq = Hash.new(0)   
+ aFile.each do |line|
+
+
+ end
+
+
+end
+
+# Now call the functions as the following.
+function1
+function2
+function3
+function4
+function5
